d034c64a0027311a7d7e3ea7a4ed31603c637dce
Eric Barnes <styxut@gmail.com>
2012-07-16 14:01:27 -0600

    add user_locations helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+    
+    def link_to_icon(icon_name, url_or_object, options={})
+        link_to(image_tag("/images/icons/#{icon_name}.png"),
+            url_or_object,
+            options)
+    end
 end
--- app/helpers/user_locations_helper.rb
+++ app/helpers/user_locations_helper.rb
@@ -1,2 +1,20 @@
 module UserLocationsHelper
+    
+    def user_locations_links(user_location)
+            content_tag :tr do
+                [   content_tag(:td, user_location.user_id), 
+                    content_tag(:td, user_location.long), 
+                    content_tag(:td, user_location.lat), 
+                    content_tag(:td, user_location.send_method), 
+                    content_tag(:td, user_location.timestamp),
+                    content_tag(:td, link_to_icon('show', user_location)), 
+                    content_tag(:td, link_to_icon('edit', edit_user_location_path(user_location))),
+                    content_tag(:td, link_to_icon('destroy', user_location, {
+                                :confirm => 'Are you sure?',
+                                :method => :delete
+                                })
+                            )
+                    ].join(' ').html_safe
+        end
+    end
 end
